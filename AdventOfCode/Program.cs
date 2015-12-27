b23f93ec2c8a7c33766d702caf80f85780655add
Ernst Fluttert <e.fluttert@gmail.com>
2015-12-27 11:28:26 +0100

    Added input file. Solved first challenge

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AdventOfCode.Challenges;
+using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace AdventOfCode
 {
@@ -11,10 +10,27 @@ namespace AdventOfCode
 	/// </summary>
 	/// <remarks>Might contain voodoo code</remarks>
 	/// <author>Ernst Fluttert / Camulos</author>
-	class Program
+	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
+			// diagnostics
+			var stopwatch = new Stopwatch(); stopwatch.Start();
+
+			// input file, prevents input clutter
+			string input = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "input.txt");
+
+			// challenge 1 + 2
+			var challenge = new C1();
+			var output = challenge.Part2(input);
+
+			stopwatch.Stop();
+			
+			// make sure some output is there with or without debugging :P
+			Console.WriteLine($"Calculation took {stopwatch.ElapsedMilliseconds} ms");
+			Console.WriteLine($"Answer: {output}");
+			Debug.WriteLine($"Calculation took {stopwatch.ElapsedMilliseconds} ms");
+			Debug.WriteLine($"Answer: {output}");
 		}
 	}
 }
