8005a300ec803f5b9c2da7c33d64bc948e7e09b1
Ernst Fluttert <e.fluttert@gmail.com>
2015-12-27 10:50:08 +0100

    Initial C#6 console program

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+	/// <summary>
+	/// The advent of code challenge
+	/// </summary>
+	/// <remarks>Might contain voodoo code</remarks>
+	/// <author>Ernst Fluttert / Camulos</author>
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
