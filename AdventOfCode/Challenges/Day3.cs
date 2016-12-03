5d379064e14bcba5fba50cb7f69376425e00c2b6
Ernst Fluttert <e.fluttert@gmail.com>
2016-12-03 16:05:42 +0100

    Day 3

---
+++ AdventOfCode/Challenges/Day3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AdventOfCode.Challenges
+{
+    public class Day3
+    {
+        public int Part1(string[] input)
+        {
+            int possibleTriangles = 0;
+            for (int index = 0; index < input.Length; index++)
+            {
+                string[] tmp = input[index].Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                int[] sides = Array.ConvertAll(tmp, int.Parse);
+                Array.Sort(sides);
+                if ((sides[0] + sides[1]) > sides[2])
+                {
+                    possibleTriangles++;
+                }
+            }
+            return possibleTriangles;
+        }
+
+        public int Part2(string[] input)
+        {
+            int possibleTriangles = 0;
+            for (int index = 0; index < input.Length; index += 3)
+            {
+                if (input.Length < (index + 2)) { continue; }
+                int[] line1 = Array.ConvertAll(input[index].Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries), int.Parse);
+                int[] line2 = Array.ConvertAll(input[index + 1].Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries), int.Parse);
+                int[] line3 = Array.ConvertAll(input[index + 2].Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries), int.Parse);
+                for (int i = 0; i < 3; i++)
+                {
+                    int[] sides = new[] { line1[i], line2[i], line3[i] };
+                    Array.Sort(sides);
+                    if ((sides[0] + sides[1]) > sides[2])
+                    {
+                        possibleTriangles++;
+                    }
+                }
+            }
+            return possibleTriangles;
+        }
+    }
+}
---
+++ AdventOfCodeTest/Challenges/Day3Tests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode.Challenges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Challenges.Tests
+{
+    [TestClass()]
+    public class Day3Tests
+    {
+        [TestMethod()]
+        public void Part1Test()
+        {
+            string[] input = { "5 10 25", "25 10 5", "5 10 12", " 10 15 5" };
+            Assert.IsTrue(new Day3().Part1(input) == 1);
+        }
+    }
+}
