e948029d8c38a051d061be4a7f6b0ef98ebbc362
Ernst Fluttert <e.fluttert@gmail.com>
2016-12-04 09:46:59 +0100

    Day 04 part 2

--- AdventOfCode/Challenges/Day04.cs
+++ AdventOfCode/Challenges/Day04.cs
@@ -18,27 +18,49 @@ namespace AdventOfCode
             return sumOfSectorIds;
         }
 
-        public int Part2(string[] input) {
-            string match = "North Pole";
-            int sectorIdOfNorthPole=0;
+        public int Part2(string[] input)
+        {
+            string match = "north";
+            // the roomname = northpole object storage
+            int sectorIdOfNorthPole = 0;
             for (int i = 0; i < input.Length; i++)
             {
                 sectorIdOfNorthPole = RealRoomSectorId(input[i]);
+
+                // 0 = decoy chamber
                 if (sectorIdOfNorthPole == 0) { continue; }
 
+                char[] encodedLine = input[i].ToCharArray();
+                for (int j = 0; j < encodedLine.Length; j++)
+                {
+                    if (encodedLine[j] == '-')
+                    {
+                        encodedLine[j] = ' ';
+                        continue;
+                    }
+                    if (encodedLine[j] < 'a' || encodedLine[j] > 'z') { continue; }
 
+                    // shift -> add the sector id, then modulo 26, while subtracting and adding a
+                    int newCharIndex = (((int)encodedLine[j] + sectorIdOfNorthPole - (int)'a') % 26) + 'a';
+                    encodedLine[j] = (char)newCharIndex;
+                }
+                string RoomName = new string(encodedLine);
+                if (RoomName.IndexOf(match) != -1)
+                {
+                    return sectorIdOfNorthPole;
+                }
             }
             return sectorIdOfNorthPole;
         }
 
-
-        private int RealRoomSectorId(string encodedData) {
+        private int RealRoomSectorId(string encodedData)
+        {
             int realRoomSectorId = 0;
             var dict = new Dictionary<char, int>();
             string sectorId = "", checkSum = String.Empty;
             int index = 0;
-            
-            // parse the string 
+
+            // parse the string
             while (index < (encodedData.Length - 1))
             {
                 char currentChar = encodedData[index];
@@ -56,7 +78,7 @@ namespace AdventOfCode
                 dict[currentChar]++;
                 index++;
             }
-            
+
             // using the power of LINQ to get the top 5 chars in alphabetical order
             var fivePopular = new string((from kp in dict orderby kp.Value descending, kp.Key ascending select kp.Key).Take(5).ToArray());
             if (fivePopular == checkSum)
