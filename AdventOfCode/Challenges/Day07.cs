3c5ba0360f54de7e1976d16ecc4d9e17060ea2c4
Ernst Fluttert <e.fluttert@gmail.com>
2016-12-07 09:33:18 +0100

    Day 7, Part 1 and 2

---
+++ AdventOfCode/Challenges/Day07.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Challenges
+{
+    public class Day07
+    {
+        public int Part1(string[] input)
+        {
+            int tlsSupported = 0;
+            foreach (string line in input)
+            {
+                int brackets = 0;
+                bool foundAbba = false, foundInBrackets=false;
+                for (int i = 0; i < (line.Length - 3); i++)
+                {
+                    if (line[i] == '[' || line[i] == ']')
+                    {
+                        brackets += line[i] == '[' ? 1 : -1;
+                        continue;
+                    }
+                    if (line[i] != line[i + 1] && line[i] == line[i + 3] && line[i + 1] == line[i + 2])
+                    {
+                        if (brackets > 0) { foundInBrackets = true; }
+                        foundAbba = true;
+                        Console.WriteLine(line.Substring(i, 4));
+                    }
+                }
+                tlsSupported += foundAbba && !foundInBrackets ? 1 : 0;
+            }
+            return tlsSupported;
+        }
+
+        public int Part2(string[] input)
+        {
+            int sslSupported = 0;
+            foreach (string line in input)
+            {
+                int brackets = 0;
+                var aba = new HashSet<string>();
+                var bab = new HashSet<string>();
+                for (int i = 0; i < (line.Length - 2); i++)
+                {
+                    if (line[i] == '[' || line[i] == ']')
+                    {
+                        brackets += line[i] == '[' ? 1 : -1;
+                        continue;
+                    }
+                    if (line[i] != line[i + 1] && line[i] == line[i + 2] )
+                    {
+                        string curSubstring = "" + line[i] + line[i + 1] + line[i + 2];
+                        string inverse = "" + line[i+1] + line[i] + line[i + 1];
+                        if (brackets == 0) {
+                            if (bab.Contains(inverse)) { sslSupported++;  break; }
+                            aba.Add(curSubstring);
+                        }
+                        else {
+                            if (aba.Contains(inverse)) { sslSupported++; break; }
+                            bab.Add(curSubstring);
+                        } 
+                    }
+                }
+            }
+            return sslSupported;
+        }
+    }
+}
---
+++ AdventOfCodeTest/Challenges/Day07Tests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Challenges.Tests
+{
+    [TestClass()]
+    public class Day07Tests
+    {
+        [TestMethod()]
+        public void Part1Test()
+        {
+            string[] input = new[] {
+                "abba[mnop]qrst",
+                "abcd[bddb]xyyx",
+                "xyyx[bddb]",
+                "aaaa[qwer]tyui",
+                "ioxxoj[asdfgh]zxcvbn",
+                "ij[asdfgh]zxcvbnoxxo",
+                "ij[oxxo]zxcvbn"
+            };
+
+            Assert.IsTrue(new Day07().Part1(input) == 3);
+        }
+
+        [TestMethod]
+        public void Part21Test()
+        {
+            string[] input = new[] {
+                "aba[bab]xyz",
+                "xyx[xyx]xyx",
+                "aaa[kek]eke",
+                "zazbz[bzb]cdb",
+                "aaa[kek]eke[kek]eke",
+            };
+
+            Assert.IsTrue(new Day07().Part2(input) == 4);
+        }
+    }
+}
