0596219ace4fdae4d3d210ff1293ea8fcbc9c379
Ernst Fluttert <e.fluttert@gmail.com>
2016-12-12 21:26:13 +0100

    Day 12, part 1 and 2

--- AdventOfCode/Challenges/Day12.cs
+++ AdventOfCode/Challenges/Day12.cs
@@ -6,15 +6,46 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode.Challenges
 {
-    class Day12
+    public class Day12
     {
-        public int Part1(string[] input) {
-
-            return 0;
+        public int Part1(string[] input, Dictionary<string, int> initRegisters = null)
+        {
+            string registernames = "abcd";
+            var registers = initRegisters; 
+            if (registers == null) { registers = new Dictionary<string, int>() { { "a", 0 }, { "b", 0 }, { "c", 0 }, { "d", 0 } }; }
+            int lineNumber = 0;
+            while (lineNumber < input.Length)
+            {
+                var parts = input[lineNumber].Trim().Split(' ');
+                string instruction = parts[0];
+                if (instruction == "inc") { registers[parts[1]]++; }
+                if (instruction == "dec") { registers[parts[1]]--; }
+                if (instruction == "cpy")
+                {
+                    int copyValue =
+                        (registernames.IndexOf(parts[1]) != -1) ?
+                        registers[parts[1]] : int.Parse(parts[1]);
+                    registers[parts[2]] = copyValue;
+                }
+                if (instruction == "jnz")
+                {
+                    int jumpNonZeroValue =
+                        (registernames.IndexOf(parts[1]) != -1) ?
+                        registers[parts[1]] : int.Parse(parts[1]);
+                    if (jumpNonZeroValue != 0)
+                    {
+                        lineNumber += int.Parse(parts[2]);
+                        continue;
+                    }
+                }
+                lineNumber++;
+            }
+            return registers["a"];
         }
 
-        public int Part2(string[] input) {
-            return 0;
+        public int Part2(string[] input)
+        {
+            return Part1(input, new Dictionary<string, int>() { { "a", 0 }, { "b", 0 }, { "c", 1 }, { "d", 0 } });
         }
     }
 }
---
+++ AdventOfCodeTest/Challenges/Day12Tests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Challenges.Tests
+{
+    [TestClass()]
+    public class Day12Tests
+    {
+        [TestMethod()]
+        public void Day12Part1Test()
+        {
+            string[] input = new[] {
+                "cpy 41 a","inc a","inc a","dec a","jnz a 2","dec a"
+            };
+            Assert.IsTrue(new Day12().Part1(input) == 42);
+            string[] input1 = new[] {
+                "cpy 2 a","inc a","dec a","jnz a -1","dec a"
+            };
+            Assert.IsTrue(new Day12().Part1(input1) == -1);
+
+        }
+    }
+}
