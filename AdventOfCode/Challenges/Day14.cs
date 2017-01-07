22021639dc4b35bd71b6849187917303a8974cbc
Ernst Fluttert <e.fluttert@gmail.com>
2017-01-07 20:16:55 +0100

    trying to get Day14 working

---
+++ AdventOfCode/Challenges/Day14.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Challenges
+{
+    public class Day14
+    {
+        internal static System.Security.Cryptography.MD5 Md5 = System.Security.Cryptography.MD5.Create();
+
+        public int Part1(string[] input)
+        {
+            string salt = input[0].Trim();
+            int currentIndex = 0;
+            int keysFound = 0;
+            string[] triplets = new string[1000];
+            string[] quintets = new string[1000];
+            string[] hashes = new string[1000];
+            
+
+            while (currentIndex < 1e+6)
+            {
+                int indexModThousand = currentIndex % 1000;
+                // search back
+                if (!string.IsNullOrEmpty(triplets[indexModThousand])) {
+                    bool keyFound = false;
+                    for (int i = 0; i < triplets[indexModThousand].Length; i++) {
+                        char curChar = triplets[indexModThousand][i];
+                        if (keyFound) { break; }
+                        for (int j = 0; j < 1000; j++) {
+                            if (quintets[j].Contains(curChar)) {
+                                //Debug.WriteLine($"Found quintent for index: {currentIndex-1000} , it was a {curChar}");
+                                keyFound = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    // keyfound
+                    if (keyFound) {
+                        keysFound++;
+                        Debug.WriteLine($"Found key for index: {currentIndex - 1000}");
+                    }
+                    if (keysFound == 64)
+                    {
+                        return currentIndex - 1000;
+                    }
+
+                }
+                if (currentIndex == 1000) {
+                    var a = 123;
+                }
+
+                // overwrite with new entry
+                string hash = BitConverter.ToString(
+                    Md5.ComputeHash(
+                        Encoding.UTF8.GetBytes(salt + currentIndex)
+                        )
+                     ).Replace("-","");
+                
+                triplets[indexModThousand] = ConsecutiveChars(hash, 3);
+                quintets[indexModThousand] = ConsecutiveChars(hash, 5);
+                currentIndex++;
+            }
+            return 0;
+        }
+
+        public string ConsecutiveChars(string input, int length)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = length - 1; i < input.Length; i++)
+            {
+                char curChar = input[i];
+                bool consecutive = true;
+                for (int j = 1; j < length; j++)
+                {
+                    if (input[i - j] != curChar)
+                    {
+                        consecutive = false;
+                        break;
+                    }
+                }
+                if (consecutive) { sb.Append(curChar); }
+            }
+            return sb.ToString();
+        }
+
+        public int Part2(string[] input)
+        {
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCodeTest/Challenges/Day14Tests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Challenges.Tests
+{
+    [TestClass()]
+    public class Day14Tests
+    {
+        [TestMethod()]
+        public void Part1Test()
+        {
+            var input = new string[] { "abc" };
+            Assert.IsTrue(new Day14().Part1(input) == 22728);
+        }
+
+        [TestMethod()]
+        public void ConsecutiveCharsTest()
+        {
+            var day14 = new Day14();
+
+            Assert.IsTrue(day14.ConsecutiveChars("aa", 2) == "a");
+            Assert.IsTrue(day14.ConsecutiveChars("aa", 3) == "");
+            Assert.IsTrue(day14.ConsecutiveChars("cc38887a5", 3) == "8");
+            Assert.IsTrue(day14.ConsecutiveChars("cc3888", 3) == "8");
+            Assert.IsTrue(day14.ConsecutiveChars("8887a5", 3) == "8");
+            Assert.IsTrue(day14.ConsecutiveChars("cc388887a5", 3) == "88");
+            Assert.IsTrue(day14.ConsecutiveChars("7EBC28B434040502706188888B77777D", 5) == "87");
+        }
+    }
+}
