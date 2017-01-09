3c1981f5e5dc8317c69f5d827104c885c71bd5ad
Ernst Fluttert <e.fluttert@gmail.com>
2017-01-09 12:04:30 +0100

    Day 16 Part 1

---
+++ AdventOfCode/Challenges/Day16.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode
+{
+    public class Day16
+    {
+        public string Part1(string[] input, int disksize = 272)
+        {
+            string data = input[0];
+            
+            // data generation
+            while (data.Length < disksize)
+            {
+                char[] extraData = new char[data.Length];
+                for (int i = 0; i < data.Length; i++)
+                {
+                    char toAdd = '1';
+                    if (data[i] == '1') { toAdd = '0'; }
+                    extraData[(data.Length - 1 - i)] = toAdd;
+                }
+                data = data + "0" + new string(extraData);
+            }
+
+            // checksum
+            List<char> checksum = new List<char>();
+            for (int i = 0; i < disksize; i++) {
+                checksum.Add(data[i]);
+            }
+            while (checksum.Count % 2 == 0) {
+                List<char> newChecksum = new List<char>();
+                for (int i = 0; i < checksum.Count; i += 2) {
+                    if (checksum[i] == checksum[i + 1])
+                    {
+                        newChecksum.Add('1');
+                    }
+                    else {
+                        newChecksum.Add('0');
+                    }
+                }
+                checksum = newChecksum;
+            }
+            return new string(checksum.ToArray());
+        }
+
+        public string Part2(string[] input)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ AdventOfCodeTest/Challenges/Day16Tests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Tests
+{
+    [TestClass()]
+    public class Day16Tests
+    {
+        [TestMethod()]
+        public void Part1Test()
+        {
+            string actual = new Day16().Part1(new string[] { "10000" }, 20);
+
+            Assert.IsTrue(actual == "01100");
+        }
+    }
+}
