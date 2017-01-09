6cb27f01146432a5eec3c285154c9ab984e9675d
Ernst Fluttert <e.fluttert@gmail.com>
2017-01-09 17:42:15 +0100

    Completed Day 17

---
+++ AdventOfCode/Challenges/Day17.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Challenges
+{
+    public class Day17
+    {
+        internal static System.Security.Cryptography.MD5 Md5 =
+            System.Security.Cryptography.MD5.Create();
+
+        public string Part1(string[] input, bool longestPath = false)
+        {
+            // tuple = X, Y, HASH
+            var q = new Queue<Tuple<int, int, string>>();
+            q.Enqueue(new Tuple<int, int, string>(0, 0, input[0]));
+            string result = string.Empty;
+            while (q.Count > 0)
+            {
+                var curItem = q.Dequeue();
+                int x = curItem.Item1;
+                int y = curItem.Item2;
+                string path = curItem.Item3;
+
+                // exit
+                if (x == 3 && y == 3)
+                {
+                    result = curItem.Item3;
+                    if (longestPath) { continue; }
+                    else { break; }
+                }
+
+                // hash is in capitals and contains dashes
+                string hash = BitConverter.ToString(
+                    Md5.ComputeHash(Encoding.UTF8.GetBytes(curItem.Item3))
+                     );
+                bool upIsOpen = hash[0] > 'A';
+                bool downIsOpen = hash[1] > 'A';
+                bool leftIsOpen = hash[3] > 'A';
+                bool rightIsOpen = hash[4] > 'A';
+
+                // process
+                if (x > 0 && leftIsOpen) { q.Enqueue(new Tuple<int, int, string>(x - 1, y, path + "L")); }
+                if (x < 3 && rightIsOpen) { q.Enqueue(new Tuple<int, int, string>(x + 1, y, path + "R")); }
+                if (y > 0 && upIsOpen) { q.Enqueue(new Tuple<int, int, string>(x, y - 1, path + "U")); }
+                if (y < 3 && downIsOpen) { q.Enqueue(new Tuple<int, int, string>(x, y + 1, path + "D")); }
+            }
+
+            result = result.Substring(input[0].Length);
+
+            return longestPath ? ""+result.Length : result;
+        }
+
+        public string Part2(string[] input)
+        {
+            return Part1(input, true);
+        }
+    }
+}
---
+++ AdventOfCodeTest/Challenges/Day17Tests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode.Challenges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Challenges.Tests
+{
+    [TestClass()]
+    public class Day17Tests
+    {
+        [TestMethod()]
+        public void Part1Test()
+        {
+            Day17 day17 = new Day17();
+            string actualTest1 = day17.Part1(new string[] { "ihgpwlah" });
+            string actualTest2 = day17.Part1(new string[] { "kglvqrro" });
+            string actualTest3 = day17.Part1(new string[] { "ulqzkmiv" });
+            Assert.IsTrue(actualTest1 == "DDRRRD");
+            Assert.IsTrue(actualTest2 == "DDUDRLRRUDRD");
+            Assert.IsTrue(actualTest3 == "DRURDRUDDLLDLUURRDULRLDUUDDDRR");
+        }
+
+        [TestMethod()]
+        public void Part2Test()
+        {
+            Day17 day17 = new Day17();
+            string actualTest1 = day17.Part2(new string[] { "ihgpwlah" });
+            string actualTest2 = day17.Part2(new string[] { "kglvqrro" });
+            string actualTest3 = day17.Part2(new string[] { "ulqzkmiv" });
+            Assert.IsTrue(actualTest1 == "370");
+            Assert.IsTrue(actualTest2 == "492");
+            Assert.IsTrue(actualTest3 == "830");
+        }
+    }
+}
