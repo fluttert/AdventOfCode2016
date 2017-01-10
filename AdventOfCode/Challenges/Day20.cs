b58a6788f98ab074385803f505bffc57e99c18a8
Ernst Fluttert <e.fluttert@gmail.com>
2017-01-10 19:23:00 +0100

    Day 20 passing

---
+++ AdventOfCode/Challenges/Day20.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Challenges
+{
+    public class Day20
+    {
+        private class IpRange
+        {
+            public long Start;
+            public long End;
+        }
+
+        public long Part1(string[] input, long maxIp = 4294967295, bool Part1 = true)
+        {
+            // sorting is the magic step here
+            var dict = new List<KeyValuePair<long, long>>();
+            foreach (var row in input) {
+                var curRow = Array.ConvertAll(row.Trim().Split('-'), long.Parse);
+                dict.Add(new KeyValuePair<long, long>(curRow[0], curRow[1]));
+            }
+            dict = dict.OrderBy(e => e.Key).ToList();
+            var blacklist = new List<IpRange>();
+
+            // stitch the ranges together
+            IpRange iprange = null;
+            for (int i = 0; i < input.Length; i++)
+            {
+                var currentBlacklistRange = dict[i];
+                if (iprange == null)
+                {
+                    iprange = new IpRange();
+                    iprange.Start = currentBlacklistRange.Key;
+                    iprange.End = currentBlacklistRange.Value;
+                    continue;
+                }
+
+                // ignore encapsulated ranges
+                if (currentBlacklistRange.Key <= iprange.End && currentBlacklistRange.Value <= iprange.End)
+                {
+                    continue;
+                }
+
+                // extend range
+                if ((currentBlacklistRange.Key <= iprange.End || (currentBlacklistRange.Key+1) == iprange.End) && currentBlacklistRange.Value > iprange.End)
+                {
+                    iprange.End = currentBlacklistRange.Value;
+                    continue;
+                }
+
+                // else start new range
+                blacklist.Add(iprange);
+                iprange = new IpRange();
+                iprange.Start = currentBlacklistRange.Key;
+                iprange.End = currentBlacklistRange.Value;
+            }
+            blacklist.Add(iprange); // add last
+
+            // loop over the ranges
+            
+            long count = 0;
+            long first = -1;
+            if (blacklist[0].Start > 0) {
+                count = blacklist[0].Start;
+                first = 0;
+            }
+
+            for (int i = 1; i < blacklist.Count; i++)
+            {
+                long gap = blacklist[i].Start - blacklist[i - 1].End -1;
+                if (gap == 0) { continue; } // no gap, just 1 difference, like end on 5 and start 6
+                if (first == -1) { first = blacklist[i - 1].End + 1; }
+                count += gap;
+            }
+            var lastBlacklistRange = blacklist[(blacklist.Count() - 1)];
+            if (lastBlacklistRange.End < maxIp) {
+                count += maxIp - lastBlacklistRange.End;
+            }
+            
+            return Part1 ? first : count;
+        }
+
+    public long Part2(string[] input, long maxIp = 4294967295)
+    {
+        return Part1(input, maxIp , false);
+    }
+}
+}
---
+++ AdventOfCodeTest/Challenges/Day20Tests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Challenges.Tests
+{
+    [TestClass()]
+    public class Day20Tests
+    {
+        [TestMethod()]
+        public void Part1Test()
+        {
+            string[] input = new[] { "5-8", "0-2", "4-7" };
+            var actual1 = new Day20().Part1(input, 10);
+            Assert.IsTrue(actual1 == 3);
+           
+        }
+
+        [TestMethod()]
+        public void Part2Test()
+        {
+            string[] input = new[] { "5-8", "0-2", "4-7" };
+            var actual1 = new Day20().Part2(input, 9);
+            Assert.IsTrue(actual1 == 2);
+
+        }
+    }
+}
